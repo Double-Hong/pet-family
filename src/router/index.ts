57381d236d50c01d3197acfe99f8aa36e1c2efcb
Yeh-Wang <wyx1627082750@163.com>
2023-07-10 20:30:17 +0800

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+        path: '/merch-grid-view',
+        name: 'MerchGridView',
+        component: () => import('../views/MerchantUser/MerchGridView.vue')
+    }
   ]
 })
 
